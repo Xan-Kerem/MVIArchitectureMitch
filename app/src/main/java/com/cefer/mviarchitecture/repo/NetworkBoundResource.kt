890067a4e8a3c86488577100dc351ae71e692051
XanKerem <jafaragha77@gmail.com>
2021-06-06 18:42:46 +0430

    NetworkBoundResource.kt added! and some little change in Repository.kt! to work with NetworkBoundResource.kt

---
+++ app/src/main/java/com/cefer/mviarchitecture/repo/NetworkBoundResource.kt
@@ -0,0 +1,67 @@
+package com.cefer.mviarchitecture.repo
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import com.cefer.mviarchitecture.util.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+abstract class NetworkBoundResource<ResponseObject , ViewStateType> {
+    
+    protected val result = MediatorLiveData<DataState<ViewStateType>>()
+    
+    init {
+        result.value = DataState.loading(true)
+        
+        GlobalScope.launch(IO) {
+            
+            delay(1000)
+            
+            withContext(Main) {
+                
+                val apiResponse = createCall()
+                
+                result.addSource(apiResponse) { genericApiResponse ->
+                    result.removeSource(apiResponse)
+                    
+                    handleNetworkCall(genericApiResponse)
+                    
+                }
+            }
+        }
+        
+    }
+    
+    private fun handleNetworkCall(response : GenericApiResponse<ResponseObject>) {
+        
+        when (response) {
+            is ApiSuccessResponse -> {
+                handleApiSuccessResponse(response)
+            }
+            
+            is ApiErrorResponse   -> {
+                println("DEBUG: NetworkBoundResource: ${response.errorMessage}")
+                onReturnError(response.errorMessage)
+            }
+            
+            is ApiEmptyResponse   -> {
+                println("DEBUG: NetworkBoundResource: Request returned NOTHING (HTTP 204)")
+                onReturnError("HTTP 204. Returned NOTHING.")
+            }
+        }
+    }
+    
+    private fun onReturnError(errorMessage : String) {
+        result.value = DataState.error(errorMessage)
+    }
+    
+    abstract fun handleApiSuccessResponse(response : ApiSuccessResponse<ResponseObject>)
+    
+    abstract fun createCall() : LiveData<GenericApiResponse<ResponseObject>>
+    
+    fun asLiveData() = result as LiveData<DataState<ViewStateType>>
+}
--- app/src/main/java/com/cefer/mviarchitecture/repo/Repository.kt
+++ app/src/main/java/com/cefer/mviarchitecture/repo/Repository.kt
@@ -1,37 +1,29 @@
 package com.cefer.mviarchitecture.repo
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
 import com.cefer.mviarchitecture.api.RetrofitBuilder
+import com.cefer.mviarchitecture.model.BlogPost
 import com.cefer.mviarchitecture.ui.main.state.MainViewState
-import com.cefer.mviarchitecture.util.ApiEmptyResponse
-import com.cefer.mviarchitecture.util.ApiErrorResponse
 import com.cefer.mviarchitecture.util.ApiSuccessResponse
 import com.cefer.mviarchitecture.util.DataState
+import com.cefer.mviarchitecture.util.GenericApiResponse
 
 object Repository {
     
     fun getBlogPosts() : LiveData<DataState<MainViewState>> {
-        return Transformations.switchMap(RetrofitBuilder.apiService.getBlogPosts()) { apiResponse ->
-            object : LiveData<DataState<MainViewState>>() {
-                override fun onActive() {
-                    super.onActive()
-                    value = when (apiResponse) {
-                        is ApiSuccessResponse -> {
-                            DataState.data(data = MainViewState(blogPosts = apiResponse.body))
-                        }
-                        
-                        is ApiErrorResponse   -> {
-                            DataState.error(message = apiResponse.errorMessage)
-                        }
-                        
-                        is ApiEmptyResponse   -> {
-                            DataState.error(message = "HTTP 204! in repo")
-                        }
-                    }
-                }
-                
+        
+        return object : NetworkBoundResource<List<BlogPost> , MainViewState>() {
+            
+            override fun handleApiSuccessResponse(response : ApiSuccessResponse<List<BlogPost>>) {
+                result.value =
+                        DataState.data(null , MainViewState(blogPosts = response.body , null))
             }
-        }
+            
+            override fun createCall() : LiveData<GenericApiResponse<List<BlogPost>>> {
+                
+                return RetrofitBuilder.apiService.getBlogPosts()
+            }
+            
+        }.asLiveData()
     }
 }
